a6fddf1973e3b362b16a17515fdcd161b58bcdf2
NgThiThanhNhu <nguyenthithanhnhu163tp@gmail.com>
2026-03-19 14:43:12 +0700

    them bai 9

---
+++ Bai9/Bai9/Program.cs
@@ -0,0 +1,35 @@
+﻿//Vẽ chữ cái H bằng dấu sao.
+for (int i=0; i<7; i++)
+{
+    Console.Write("*");
+    if (i == 3)
+    {
+        for (int j = 0; j < 4; j++)
+        {
+            Console.Write("*");
+        }
+
+    }
+    else
+    {
+        Console.Write("    ");
+    }
+    Console.WriteLine("*");
+    
+}
+
+//Vẽ chữ cái O bằng dấu sao
+Console.WriteLine("Ve chu O");
+for (int i=0; i<7; i++)
+{
+    if (i==0 || i== 6)
+    {
+        Console.WriteLine(" *** ");
+    }
+    else
+    {
+        Console.WriteLine("*   *");
+    }
+    
+}
+
